ecccaf5c381f5a638cc0dc29f267c795c8dfeae0
AlexLiber <liberaleksa@gmail.com>
2018-04-19 23:35:30 +0200

    Create comments in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+// this is a short comment
+
+/* this is
+a comment
+which is
+a little bit longer */
